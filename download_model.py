60262a0eddf390bc771b5380561597f5fa4a65e6
Aleksei Žavoronkov <aleksei.zavoronkov@taltech.ee>
2026-04-10 16:25:12 +0300

    update model download and image building code

--- download_model.py
+++ download_model.py
@@ -6,43 +6,30 @@ logger = logging.getLogger(__name__)
 
 
 def download_and_save_model_snapshot():
-    QUALITY_MODEL_REPO_ID = os.getenv("QUALITY_MODEL_REPO_ID")
-    DURATION_MODEL_REPO_ID = os.getenv("DURATION_MODEL_REPO_ID")
-    QUALITY_MODEL_PATH = os.getenv("QUALITY_MODEL_PATH")
-    DURATION_MODEL_PATH = os.getenv("DURATION_MODEL_PATH")
+    model_repo_id = os.getenv("MODEL_REPO_ID")
+    model_path = os.getenv("MODEL_PATH")
 
-    if not all(
-        [
-            QUALITY_MODEL_REPO_ID,
-            QUALITY_MODEL_PATH,
-            DURATION_MODEL_REPO_ID,
-            DURATION_MODEL_PATH,
-        ]
-    ):
-        error_msg = "All model environment variables (REPO_ID and PATH) must be set."
+    if not all([model_repo_id, model_path]):
+        error_msg = "MODEL_REPO_ID and MODEL_PATH must be set."
         logger.error(error_msg)
         raise ValueError(error_msg)
 
     ignore_patterns = ["*.h5", "*.ot", "*.msgpack"]
 
-    for model_id, model_path in [
-        (QUALITY_MODEL_REPO_ID, QUALITY_MODEL_PATH),
-        (DURATION_MODEL_REPO_ID, DURATION_MODEL_PATH),
-    ]:
-        logger.info(f"Starting download for model: {model_id}")
-        logger.info(f"Model will be saved to: {model_path}")
+    logger.info(f"Starting download for model: {model_repo_id}")
+    logger.info(f"Model will be saved to: {model_path}")
 
-        try:
-            snapshot_download(
-                repo_id=model_id,
-                local_dir=model_path,
-                ignore_patterns=ignore_patterns,
-            )
-            logger.info(f"Successfully downloaded {model_id}")
+    try:
+        snapshot_download(
+            repo_id=model_repo_id,
+            local_dir=model_path,
+            ignore_patterns=ignore_patterns,
+        )
+        logger.info(f"Successfully downloaded {model_repo_id}")
 
-        except Exception as e:
-            logger.error(f"Failed to download {model_id}. Error: {e}")
-            raise
+    except Exception as e:
+        logger.error(f"Failed to download {model_repo_id}. Error: {e}")
+        raise
 
 
 if __name__ == "__main__":
